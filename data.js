3a4075a5aebe0ec427611691aa8d4f7f4ce73ef0
unknown <614259004@webmail.npru.ac.th>
2020-10-04 16:39:25 +0700

    Initial portfolio project Commit

---
+++ data.js
@@ -0,0 +1,23 @@
+const data = {
+    "name" : "Jirayuk Sae-lim",
+    "work" : "University student",
+    "aboutme" :{
+        "FullName" : "Jirayuk Sae-lim",
+        "Age" : "20",
+        "B-date" : "5/5/2000",
+        "Phone" : "0614711894",
+        "Email" : "614259004@webmail.npru.ac.th",
+        "Address" : "Nakhon Pathom Rajaphat University 85 Malaiman Road",
+        "Address2" : ", Muang District,Nakhon Pathom 73000 Thailand"
+    },
+    "welcome" : "This website is my portfolio. ",
+    "welcome2" :"Made for education in academic web services Thank you for watching.",
+    "school" : "Annalai School",
+    "university" : "Nakhon Pathom Rajaphat University",
+    "workexperience" : "I don't have workexperience.Sorry man",
+    "css" : "45%",
+    "php" : "30%",
+    "html" : "50%",
+    "js" : "10%"
+};
+module.exports = data;
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = require('./routes/index');
+const hbs = require('express-handlebars');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+//Middleware
+app.use(express.static(path.join(__dirname,'public')));
+//Template engine 
+app.engine('hbs',hbs({extname:'hbs'}));
+app.set('view engine', 'hbs');
+
+//router
+app.use('/profile', router);
+
+app.listen(
+    PORT,
+    () => {
+      console.log(`Listening to port ${PORT}`);
+    }
+  );
+  
